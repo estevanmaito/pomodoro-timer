e22a446da707662071fc42c1b3e8b50be5bd71bb
Estevan Maito <ejmaito@gmail.com>
2017-02-21 17:37:02 -0300

    sw change files directory

--- service-worker.js
+++ service-worker.js
@@ -5,12 +5,12 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('pomodoro-cache-v1').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/src/assets/audio/beep.mp3',
-        '/src/assets/css/style.css',
-        '/src/assets/font/fontello.woff2',
-        '/favicon.png',
-        '/dist/build.js'
+        './',
+        './src/assets/audio/beep.mp3',
+        './src/assets/css/style.css',
+        './src/assets/font/fontello.woff2',
+        './favicon.png',
+        './dist/build.js'
       ]);
     })
   );
